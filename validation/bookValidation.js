8d2b1bb1f6b511c87c69a3d04967fe51553b63ab
Ekg23 <geoffreyetu56@gmail.com>
2026-05-27 14:46:27 +0000

    Project2 books collections few changes made

--- validation/bookValidation.js
+++ validation/bookValidation.js
@@ -4,7 +4,7 @@ const validateBook = [
     body('title').isString().notEmpty().withMessage('Title is required and must be a string.'),
     body('author').isString().notEmpty().withMessage('Author is required and must be a string.'),
     body('genre').isString().notEmpty().withMessage('Genre is required and must be a string.'),
-    body('publicationYear').isString().notEmpty().withMessage('Publication year is required and must be a string.')
+    body('publishedYear').isString().notEmpty().withMessage('Published year is required and must be a string.')
 ]; 
 
 module.exports = {
